7047ea85d56ac4867f12dc527a94fc77a139777c
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-14 14:16:32 +0000

    chore: ajustes no build.gradle e inicialização da classe principal

---
+++ src/main/java/com/voxreal/VoxRealApplication.java
@@ -0,0 +1,11 @@
+package com.voxreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoxRealApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VoxRealApplication.class, args);
+    }
+}
